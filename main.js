d39734032275442082085d1f44b4e944dbe78b2b
Daiki Tomioka <chocolate0785lego@icloud.com>
2020-06-14 02:11:22 +0900

    fix 2 space tab indent

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 let intervalId = null;
 
 const tokyoAlert = function () {
-        window.alert("東京");
+  window.alert("東京");
 };
 
 const releaseTokyo = function () {
-        window.alert("解除");
+  window.alert("解除");
 };
 
 const trigger = function () {
-        // document.body.style.backgroundColor = "red";
-        body.classList.add('state1');
-        body.classList.remove('state2');
-        tokyoAlert();
-        intervalId = setInterval(tokyoAlert, 1500);
+  // document.body.style.backgroundColor = "red";
+  body.classList.add('state1');
+  body.classList.remove('state2');
+  tokyoAlert();
+  intervalId = setInterval(tokyoAlert, 1500);
 };
 
 const end = function () {
-        // document.body.style.backgroundColor = "white";
-        body.classList.add('state2');
-        body.classList.remove('state1');
-        clearInterval(intervalId);
-        intervalId = null;
-        releaseTokyo();
+  // document.body.style.backgroundColor = "white";
+  body.classList.add('state2');
+  body.classList.remove('state1');
+  clearInterval(intervalId);
+  intervalId = null;
+  releaseTokyo();
 };
